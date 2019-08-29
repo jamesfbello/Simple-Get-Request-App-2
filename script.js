41e9f6f315c1d0e2660d96241a263527928dba0d
jbello1222 <50038804+jbello1222@users.noreply.github.com>
2019-08-28 23:12:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+"use strict";
+//Pass numInput, which represents an integer as an argument
+function getDogImage(numInput) {
+  if  (numInput > 50) {
+      return alert("Please choose a number equal to or less than 50");
+  }
+  else {
+      fetch (`https://dog.ceo/api/breeds/image/random/${numInput}`)
+        .then(response => response.json())
+        .then(responseJson => displayResults(responseJson))
+        .catch(error => alert('Something went wrong. Try again later.'));
+  }
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  //replace the existing image with the new one
+  // Option 1
+  $('.results-img').replaceWith(
+    `<img src="${responseJson.message}" class="results-img">`
+  )
+  // Option 2
+  // $('.results-img').html('');
+  // responseJson.message.forEach(renderedImg => {
+  //   $('.results-img').append(`<img src="${renderedImg}" class="results-img">`)
+  // });
+
+  //display the results section
+  $('.results').removeClass('hidden');
+}
+
+function watchUserInput() {
+  $("#dog-num-form").submit(e => {
+    event.preventDefault();
+    let userNumInput = $("#num-dog").val();
+    //Pass the number value to getDogImage
+    getDogImage(userNumInput);
+  });
+}
+
+$(function() {
+  console.log("App loaded! Waiting for submit!");
+  watchUserInput();
+});
+
